b67e7466d49ffc47b1876bd9cac7c3c59f248abe
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2023-06-04 01:21:38 +0700

    chore: cloud run

---
+++ config.js
@@ -0,0 +1,4 @@
+// Import all env vars from .env file
+
+export const PUBLIC_SUPABASE_PROJECT = import.meta.env.PUBLIC_SUPABASE_PROJECT;
+export const PUBLIC_SUPABASE_API_KEY = import.meta.env.PUBLIC_SUPABASE_API_KEY;
