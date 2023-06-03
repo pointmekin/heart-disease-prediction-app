d82091434b0f0f897da167217ac04789a8c753df
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2023-06-04 01:35:05 +0700

    chore: add VITE prefix

--- src/lib/db/client.ts
+++ src/lib/db/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-import { PUBLIC_SUPABASE_PROJECT, PUBLIC_SUPABASE_API_KEY } from "$env/static/public";
+// import { PUBLIC_SUPABASE_PROJECT, PUBLIC_SUPABASE_API_KEY } from "$env/static/public";
 
 export const supabase = createClient(
-	`https://${PUBLIC_SUPABASE_PROJECT}.supabase.co`,
-	PUBLIC_SUPABASE_API_KEY
+	`https://${import.meta.env.VITE_PUBLIC_SUPABASE_PROJECT}.supabase.co`,
+	import.meta.env.VITE_PUBLIC_SUPABASE_API_KEY
 );
